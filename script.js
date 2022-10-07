62c35c51c81d8c8caae6911ad37fb3ff45e69ef8
Fserver <server-77@outlook.com>
2022-10-07 16:13:37 -0500

    index completo

--- script.js
+++ script.js
@@ -26,7 +26,7 @@ const alert = (message, type) => {
     alertPlaceholder.append(wrapper)
 }
 
-function buscarAfiliado() {
+function inicioSesion() {
     event.preventDefault()
 
     inicioUser = document.getElementById('inicioUser').value
@@ -39,7 +39,13 @@ function buscarAfiliado() {
 
         if (registroEncontrado) {
             if (registroEncontrado.numeroDocumento == inicioUser && registroEncontrado.password == inicioPassword) {
-                usuarioLogueado = registroEncontrado.nombre
+                usuarioLogueado = {
+                    id: registroEncontrado.numeroDocumento,
+                    user: registroEncontrado.nombre
+                }
+                sessionStorage.setItem('sesion', JSON.stringify(usuarioLogueado))
+
+                console.log(registroEncontrado.nombre);
                 window.location.href = "index.html"
             } else {
                 alert("Datos incorrectos.", "danger")
@@ -80,70 +86,83 @@ function registrarAfiliado() {
     } else alert('Debes llenar cada uno de los campos', 'warning')
 }
 
+//COMPROBADOR DE SESIÓN
+(function name() {
 
+    if (window.location.pathname != "/login.html") {
+        usuarioLogueado = JSON.parse(sessionStorage.getItem('sesion'))
 
+        if (usuarioLogueado != null) {
+            document.getElementById('usuarioActivo').innerText = "Hola, "+usuarioLogueado.user
+        } else window.location.href = "login.html"
+    }
+})()
 
-
-
-
-
-function eliminarCliente() {
-    event.preventDefault()
-
-    campoCedula = document.getElementById('campoCedula').value
-
-    if (campoCedula !== "") {
-        listaClientes = leerLocalStorage()
-
-        let registroEncontrado = listaClientes.find(elemento => elemento.cedula == campoCedula)
-
-        if (registroEncontrado !== undefined) {
-            listaClientes = listaClientes.filter(registro => registro.cedula !== registroEncontrado.cedula)
-
-            localStorage.clear()
-            localStorage.setItem(0, JSON.stringify(listaClientes))
-
-            alert(registroEncontrado.nombre + " fue eliminado.");
-        } else {
-            document.getElementById('campoNombre').value = ""
-            alert("Este cliente No existe.")
-        }
-    } else alert("Debe digitar una cédula existente para eliminar.")
-
-    document.getElementById('formularioClientes').reset()
+function destruirSesion() {
+    sessionStorage.clear()
+    window.location.href = "login.html"
 }
 
-function actualizarCliente() {
-    event.preventDefault()
 
-    campoCedula = document.getElementById('campoCedula').value
 
-    if (campoCedula !== "") {
-        listaClientes = leerLocalStorage()
 
-        let registroEncontrado = listaClientes.find(elemento => elemento.cedula == campoCedula)
+// function eliminarCliente() {
+//     event.preventDefault()
 
-        if (registroEncontrado !== undefined) {
+//     campoCedula = document.getElementById('campoCedula').value
 
-            campoNombre = document.getElementById('campoNombre').value
+//     if (campoCedula !== "") {
+//         listaClientes = leerLocalStorage()
 
-            alert(registroEncontrado.nombre + " fue actualizado a: " + campoNombre);
-            registroEncontrado.nombre = campoNombre
+//         let registroEncontrado = listaClientes.find(elemento => elemento.cedula == campoCedula)
 
-            console.log(registroEncontrado);
-            console.log(listaClientes);
+//         if (registroEncontrado !== undefined) {
+//             listaClientes = listaClientes.filter(registro => registro.cedula !== registroEncontrado.cedula)
 
-            localStorage.clear()
-            localStorage.setItem(0, JSON.stringify(listaClientes))
+//             localStorage.clear()
+//             localStorage.setItem(0, JSON.stringify(listaClientes))
 
-        } else {
-            document.getElementById('campoNombre').value = ""
-            alert("Este cliente No existe.")
-        }
-    } else alert("Debe llenar ambos campos para Actualizar.")
+//             alert(registroEncontrado.nombre + " fue eliminado.");
+//         } else {
+//             document.getElementById('campoNombre').value = ""
+//             alert("Este cliente No existe.")
+//         }
+//     } else alert("Debe digitar una cédula existente para eliminar.")
 
-    document.getElementById('formularioClientes').reset()
-}
+//     document.getElementById('formularioClientes').reset()
+// }
+
+// function actualizarCliente() {
+//     event.preventDefault()
+
+//     campoCedula = document.getElementById('campoCedula').value
+
+//     if (campoCedula !== "") {
+//         listaClientes = leerLocalStorage()
+
+//         let registroEncontrado = listaClientes.find(elemento => elemento.cedula == campoCedula)
+
+//         if (registroEncontrado !== undefined) {
+
+//             campoNombre = document.getElementById('campoNombre').value
+
+//             alert(registroEncontrado.nombre + " fue actualizado a: " + campoNombre);
+//             registroEncontrado.nombre = campoNombre
+
+//             console.log(registroEncontrado);
+//             console.log(listaClientes);
+
+//             localStorage.clear()
+//             localStorage.setItem(0, JSON.stringify(listaClientes))
+
+//         } else {
+//             document.getElementById('campoNombre').value = ""
+//             alert("Este cliente No existe.")
+//         }
+//     } else alert("Debe llenar ambos campos para Actualizar.")
+
+//     document.getElementById('formularioClientes').reset()
+// }
 
 // (function () {
 //     listaClientes = leerLocalStorage()
@@ -204,138 +223,138 @@ function actualizarCliente() {
 
 
 //Aqui se esconde como imprimir todas las ventas por 1 cliente
-function buscarVenta() {
-    event.preventDefault()
+// function buscarVenta() {
+//     event.preventDefault()
 
-    selectCliente = document.getElementById('selectClientes').value
-    //console.log(selectCliente);
+//     selectCliente = document.getElementById('selectClientes').value
+//     //console.log(selectCliente);
 
-    campoID = document.getElementById('idVentas').value
+//     campoID = document.getElementById('idVentas').value
 
-    if (campoID !== "") {
-        listaClientes = leerLocalStorage()
+//     if (campoID !== "") {
+//         listaClientes = leerLocalStorage()
 
-        let registroEncontrado = listaClientes.find(elemento => elemento.cedula == selectCliente)
-        //console.log(registroEncontrado);
-        let ventaEncontrada = registroEncontrado.ventas.find(element => element.id == campoID)
-        //console.log(ventaEncontrada);
+//         let registroEncontrado = listaClientes.find(elemento => elemento.cedula == selectCliente)
+//         //console.log(registroEncontrado);
+//         let ventaEncontrada = registroEncontrado.ventas.find(element => element.id == campoID)
+//         //console.log(ventaEncontrada);
 
-        if (ventaEncontrada != undefined) {
-            campoProducto = document.getElementById('producto').value = ventaEncontrada.producto
-            campoFecha = document.getElementById('fecha').value = ventaEncontrada.fecha
-            campoValor = document.getElementById('valor').value = ventaEncontrada.valor
-        } else {
-            campoID = document.getElementById('idVentas').value = ""
-            alert("Ese registro no existe")
-        }
-    } else alert("El campo ID es requerido.")
-}
+//         if (ventaEncontrada != undefined) {
+//             campoProducto = document.getElementById('producto').value = ventaEncontrada.producto
+//             campoFecha = document.getElementById('fecha').value = ventaEncontrada.fecha
+//             campoValor = document.getElementById('valor').value = ventaEncontrada.valor
+//         } else {
+//             campoID = document.getElementById('idVentas').value = ""
+//             alert("Ese registro no existe")
+//         }
+//     } else alert("El campo ID es requerido.")
+// }
 
-function registrarVenta() {
-    event.preventDefault()
+// function registrarVenta() {
+//     event.preventDefault()
 
-    selectCliente = document.getElementById('selectClientes').value
-    //console.log(selectCliente);
+//     selectCliente = document.getElementById('selectClientes').value
+//     //console.log(selectCliente);
 
-    //campoID = document.getElementById('idVentas').value
-    campoProducto = document.getElementById('producto').value
-    campoFecha = document.getElementById('fecha').value
-    campoValor = document.getElementById('valor').value
+//     //campoID = document.getElementById('idVentas').value
+//     campoProducto = document.getElementById('producto').value
+//     campoFecha = document.getElementById('fecha').value
+//     campoValor = document.getElementById('valor').value
 
-    if (campoProducto !== "" && campoFecha !== "" && campoValor !== "") {
-        listaClientes = leerLocalStorage()
+//     if (campoProducto !== "" && campoFecha !== "" && campoValor !== "") {
+//         listaClientes = leerLocalStorage()
 
-        let registroEncontrado = listaClientes.find(elemento => elemento.cedula == selectCliente)
-        //console.log(registroEncontrado);
+//         let registroEncontrado = listaClientes.find(elemento => elemento.cedula == selectCliente)
+//         //console.log(registroEncontrado);
 
-        let cantidadElementosVentas = registroEncontrado.ventas.length
-        console.log("Cantidad de ventas: ", cantidadElementosVentas + 1);
+//         let cantidadElementosVentas = registroEncontrado.ventas.length
+//         console.log("Cantidad de ventas: ", cantidadElementosVentas + 1);
 
-        try {
-            ultimoElementoContador = registroEncontrado.ventas[cantidadElementosVentas - 1].id
-            console.log("Ultimo id: " + ultimoElementoContador);
-        } catch (error) { }
+//         try {
+//             ultimoElementoContador = registroEncontrado.ventas[cantidadElementosVentas - 1].id
+//             console.log("Ultimo id: " + ultimoElementoContador);
+//         } catch (error) { }
 
-        if (cantidadElementosVentas <= 0) {
-            registroEncontrado.ventas.push({
-                id: 0,
-                producto: campoProducto,
-                fecha: campoFecha,
-                valor: campoValor
-            },)
-        } else if (cantidadElementosVentas > 0) {
-            registroEncontrado.ventas.push({
-                id: ultimoElementoContador + 1,
-                producto: campoProducto,
-                fecha: campoFecha,
-                valor: campoValor
-            },)
-        }
+//         if (cantidadElementosVentas <= 0) {
+//             registroEncontrado.ventas.push({
+//                 id: 0,
+//                 producto: campoProducto,
+//                 fecha: campoFecha,
+//                 valor: campoValor
+//             },)
+//         } else if (cantidadElementosVentas > 0) {
+//             registroEncontrado.ventas.push({
+//                 id: ultimoElementoContador + 1,
+//                 producto: campoProducto,
+//                 fecha: campoFecha,
+//                 valor: campoValor
+//             },)
+//         }
 
-        localStorage.clear()
-        localStorage.setItem(0, JSON.stringify(listaClientes))
-        alert("Se ha registrado la venta.")
+//         localStorage.clear()
+//         localStorage.setItem(0, JSON.stringify(listaClientes))
+//         alert("Se ha registrado la venta.")
 
-        //console.log(listaClientes);
+//         //console.log(listaClientes);
 
-        document.getElementById('formularioVentas').reset()
-    } else alert("Debe llenar los campos para registrar una venta.")
-}
+//         document.getElementById('formularioVentas').reset()
+//     } else alert("Debe llenar los campos para registrar una venta.")
+// }
 
-function actualizarVenta() {
-    event.preventDefault()
+// function actualizarVenta() {
+//     event.preventDefault()
 
-    selectCliente = document.getElementById('selectClientes').value
-    //console.log(selectCliente);
+//     selectCliente = document.getElementById('selectClientes').value
+//     //console.log(selectCliente);
 
-    campoID = document.getElementById('idVentas').value
+//     campoID = document.getElementById('idVentas').value
 
-    if (campoID !== "") {
-        listaClientes = leerLocalStorage()
+//     if (campoID !== "") {
+//         listaClientes = leerLocalStorage()
 
-        let registroEncontrado = listaClientes.find(elemento => elemento.cedula == selectCliente)
-        //console.log(registroEncontrado);
-        let ventaEncontrada = registroEncontrado.ventas.find(element => element.id == campoID)
-        //console.log(ventaEncontrada);
+//         let registroEncontrado = listaClientes.find(elemento => elemento.cedula == selectCliente)
+//         //console.log(registroEncontrado);
+//         let ventaEncontrada = registroEncontrado.ventas.find(element => element.id == campoID)
+//         //console.log(ventaEncontrada);
 
-        if (ventaEncontrada != undefined) {
-            ventaEncontrada.producto = document.getElementById('producto').value
-            ventaEncontrada.fecha = document.getElementById('fecha').value
-            ventaEncontrada.valor = document.getElementById('valor').value
+//         if (ventaEncontrada != undefined) {
+//             ventaEncontrada.producto = document.getElementById('producto').value
+//             ventaEncontrada.fecha = document.getElementById('fecha').value
+//             ventaEncontrada.valor = document.getElementById('valor').value
 
-            localStorage.clear()
-            localStorage.setItem(0, JSON.stringify(listaClientes))
-            alert("Se ha realizado la actualización.")
-        } else {
-            campoID = document.getElementById('idVentas').value = ""
-            alert("Ese registro no existe")
-        }
-    } else alert("El campo ID es requerido.")
-}
+//             localStorage.clear()
+//             localStorage.setItem(0, JSON.stringify(listaClientes))
+//             alert("Se ha realizado la actualización.")
+//         } else {
+//             campoID = document.getElementById('idVentas').value = ""
+//             alert("Ese registro no existe")
+//         }
+//     } else alert("El campo ID es requerido.")
+// }
 
-function eliminarVenta() {
-    event.preventDefault()
+// function eliminarVenta() {
+//     event.preventDefault()
 
-    selectCliente = document.getElementById('selectClientes').value
-    //console.log(selectCliente);
+//     selectCliente = document.getElementById('selectClientes').value
+//     //console.log(selectCliente);
 
-    campoID = document.getElementById('idVentas').value
+//     campoID = document.getElementById('idVentas').value
 
-    if (campoID !== "") {
-        listaClientes = leerLocalStorage()
+//     if (campoID !== "") {
+//         listaClientes = leerLocalStorage()
 
-        let registroEncontrado = listaClientes.find(elemento => elemento.cedula == selectCliente)
-        //console.log(registroEncontrado);
-        if (registroEncontrado) {
+//         let registroEncontrado = listaClientes.find(elemento => elemento.cedula == selectCliente)
+//         //console.log(registroEncontrado);
+//         if (registroEncontrado) {
 
-            registroEncontrado.ventas = registroEncontrado.ventas.filter(element => element.id != campoID)
+//             registroEncontrado.ventas = registroEncontrado.ventas.filter(element => element.id != campoID)
 
-            localStorage.clear()
-            localStorage.setItem(0, JSON.stringify(listaClientes))
+//             localStorage.clear()
+//             localStorage.setItem(0, JSON.stringify(listaClientes))
 
-            document.getElementById('formularioVentas').reset()
+//             document.getElementById('formularioVentas').reset()
 
-            alert("Se ha eliminado el registro.")
-        }
-    } else alert("El campo ID es requerido.")
-}
+//             alert("Se ha eliminado el registro.")
+//         }
+//     } else alert("El campo ID es requerido.")
+// }
